78baaa7b19467c2368d1b65e5d15efa847a68ac0
BackstreetCultist <charlie@101a.co.uk>
2020-02-15 19:10:05 +0000

    Add first draft of hardware code

--- Hardware/main.py
+++ Hardware/main.py
@@ -2,8 +2,32 @@ from time import sleep
 import RPi.GPIO as GPIO
 import asyncio
 import websockets
+import constant
 
-GPIO.setmode(GPIO.BCM)
+def setGPIOs():
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setup(UPMOTOR, GPIO.OUT)
+    GPIO.setup(LEFTEYE, GPIO.OUT)
+    GPIO.setup(RIGHTEYE, GPIO.OUT)
+    GPIO.setup(DOWNMOTOR, GPIO.OUT)
+
+def act():
+        GPIO.output(UPMOTOR, False)
+        sleep(0.50)
+        GPIO.output(UPMOTOR, True)
+
+        GPIO.output(LEFTEYE, True)
+        GPIO.output(RIGHTEYE, True)
+        sleep(5.00)
+        GPIO.output(LEFTEYE, False)
+        GPIO.output(RIGHTEYE, False)
+
+        GPIO.output(DOWNMOTOR, False)
+        sleep(0.50)
+        GPIO.output(DOWNMOTOR, True)
+
+
+
 
 async def start():
     uri = "ws://lookalivesunshine.tech:81"
@@ -11,12 +35,10 @@ async def start():
         await websocket.send("pi")
         await websocket.recv()
 
-        interval = 0.02
-
-        GPIO.setup(2, GPIO.OUT)
-        GPIO.output(2, False)
-        sleep(interval)
-        GPIO.cleanup()
+        print("Pi working")
+        act()
 
 
+setGPIOs()
 asyncio.get_event_loop().run_until_complete(start())
+GPIO.cleanup()
