e5f483b82d92a69c4ddb4242ae894f91954895a8
psycaj <psycaj@nottingham.ac.uk>
2020-02-16 09:17:30 +0000

    initial image generation complete

--- PropagandaAI/SloganGenerator.py
+++ PropagandaAI/SloganGenerator.py
@@ -1,16 +1,25 @@
 import markovify
+import random 
+from PIL import Image
+from PIL import ImageFont
+from PIL import ImageDraw 
 
 numberOfWords = 30
-numberOfSentences = 50
+numberOfSentences = 10
 numberOfGoodSentences = 0
 
 slogans = [['' for x in  range(numberOfWords)] for y in range(numberOfSentences)]
 
 def Original(slogan):
-    current = set(slogan.split(" "))
+    current = slogan.split(" ")
     for i in slogans:
-        if len(current & set(i)) >= len(current):
-            return False
+        counter = 0
+        for j in i:
+            for k in current:
+                if k == j:
+                    counter += 1
+            if counter > len(current)/2:
+                return False
     return True
 
 # Get raw text as string.
@@ -28,5 +37,13 @@ while (numberOfGoodSentences < numberOfSentences):
         slogans[numberOfGoodSentences] = slogan.split(" ")
         numberOfGoodSentences += 1
 
-for i in list(map(lambda x: " ".join(x), slogans)):
-    print(i)
+slogans = list(map(lambda x: " ".join(x), slogans))
+
+imageID = random.randint(0, 16)
+background = Image.open("famous/background.png")
+image = Image.open('famous/image'+str(imageID)+'.png')
+background.paste(image, (0, 300), image)
+font = ImageFont.truetype("/Library/Fonts/Impact.ttf", 100)
+draw = ImageDraw.Draw(background)
+draw.text((10, 0),slogans[0],(0,0,80),font=font)
+background.show()
--- PropagandaAI/propagandaGenerator.py
+++ PropagandaAI/propagandaGenerator.py
@@ -3,11 +3,11 @@ from PIL import Image
 from PIL import ImageFont
 from PIL import ImageDraw 
 
-imageID = random.randint(0, 10)
-background = Image.open("test.png")
-image = Image.open("monkey.png")
-background.paste(image, (0, 0), image)
-font = ImageFont.truetype("/Library/Fonts/Arial.ttf", 500)
+imageID = random.randint(0, 16)
+background = Image.open("famous/background.png")
+image = Image.open('famous/image'+str(imageID)+'.png')
+background.paste(image, (0, 300), image)
+font = ImageFont.truetype("/Library/Fonts/Arial.ttf", 100)
 draw = ImageDraw.Draw(background)
-draw.text((0, 0),"Propaganda",(240,10,10),font=font)
+draw.text((0, 0),"Propaganda",(0,0,80),font=font)
 background.show()
