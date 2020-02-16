a9d0953175f572bafc251a13d04cc49485296799
psysm12 <psysm12@exmail.nottingham.ac.uk>
2020-02-16 07:26:31 +0000

    added websocket server

--- index.js
+++ index.js
@@ -1,15 +1,19 @@
+/* Discord API */
 const Discord = require("discord.js");
+const config = require('./auth.json');
 const client = new Discord.Client();
 
-const config = require('./auth.json');
+/* Google API */ 
 const language = require("@google-cloud/language")
 const googleclient = new language.LanguageServiceClient();
-
 const wordList = require("./bannedWords.json")
-
 const threshold = 0.4
 
-async function getSentiment(text) 
+/* Websocket server */
+const WebSocket = require("ws");
+const wss = new WebSocket.Server({port : process.env.PORT || 8080});
+
+async function getSentiment(text)
 {
 
   var document = {
@@ -25,7 +29,7 @@ async function getSentiment(text)
   badwords = []
   entities.forEach(entity => {
     if (wordList['bad'].includes(entity.name) && entity.sentiment.score >= threshold )
-    { 
+    {
       console.log(`  Name: ${entity.name}`);
       console.log(`  Score: ${entity.sentiment.score}`);
       console.log(`  Magnitude: ${entity.sentiment.magnitude}`);
@@ -33,19 +37,36 @@ async function getSentiment(text)
     }
   });
 
+
   return badwords;
 }
 
-client.login(config.token);
 
 client.on('message', async function (message) {
   var badwords = await getSentiment(message.content)
-  if (badwords.length > 0) 
+  if (badwords.length > 0)
   {
     message.react('👎')
+    // message doesn't matter
+    if (socket) {socket.send('now');}
   }
 })
 
 client.on('ready', () => {
   console.log('The ear hears all...');
 });
+
+
+
+var socket = null;
+wss.on('connection', (ws)=>{
+  console.log("pi connected");
+  socket = ws; 
+}); 
+
+wss.on('close', (ws) => {
+  console.log("pi disconnected");
+  socket=null
+});
+
+client.login(config.token);
