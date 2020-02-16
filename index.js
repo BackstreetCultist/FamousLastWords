6f468617e93e68c9cfd51dc26b98b7d5457914d4
psysm12 <psysm12@exmail.nottingham.ac.uk>
2020-02-16 05:56:06 +0000

    added banned words and reacts

--- index.js
+++ index.js
@@ -5,30 +5,45 @@ const config = require('./auth.json');
 const language = require("@google-cloud/language")
 const googleclient = new language.LanguageServiceClient();
 
+const wordList = require("./bannedWords.json")
+
+const threshold = 0.4
+
 async function getSentiment(text) 
 {
 
-var document = {
-    content: text,
-    type: 'PLAIN_TEXT'
-}
+  var document = {
+      content: text,
+      type: 'PLAIN_TEXT'
+  }
 
-var [result] = await googleclient.analyzeEntitySentiment({document});
-var entities = result.entities;
+  var [result] = await googleclient.analyzeEntitySentiment({document});
+  var entities = result.entities;
 
-console.log(`Entities and sentiments:`);
+  console.log(`Entities and sentiments:`);
 
-entities.forEach(entity => {
-    console.log(`  Name: ${entity.name}`);
-    console.log(`  Score: ${entity.sentiment.score}`);
-    console.log(`  Magnitude: ${entity.sentiment.magnitude}`);
+  badwords = []
+  entities.forEach(entity => {
+    if (wordList['bad'].includes(entity.name) && entity.sentiment.score >= threshold )
+    { 
+      console.log(`  Name: ${entity.name}`);
+      console.log(`  Score: ${entity.sentiment.score}`);
+      console.log(`  Magnitude: ${entity.sentiment.magnitude}`);
+      badwords.push(entity)
+    }
   });
+
+  return badwords;
 }
 
 client.login(config.token);
 
 client.on('message', async function (message) {
-  await getSentiment(message.content)
+  var badwords = await getSentiment(message.content)
+  if (badwords.length > 0) 
+  {
+    message.react('👎')
+  }
 })
 
 client.on('ready', () => {
