292df1142418e64d1c4854de3dbb8ce22ee368ae
psysm12 <psysm12@exmail.nottingham.ac.uk>
2020-02-16 05:01:12 +0000

    completely merged googel cloud code with bot

--- index.js
+++ index.js
@@ -2,11 +2,33 @@ const Discord = require("discord.js");
 const client = new Discord.Client();
 
 const config = require('./auth.json');
+const language = require("@google-cloud/language")
+const googleclient = new language.LanguageServiceClient();
+
+async function getSentiment(text) 
+{
+
+var document = {
+    content: text,
+    type: 'PLAIN_TEXT'
+}
+
+var [result] = await googleclient.analyzeEntitySentiment({document});
+var entities = result.entities;
+
+console.log(`Entities and sentiments:`);
+
+entities.forEach(entity => {
+    console.log(`  Name: ${entity.name}`);
+    console.log(`  Score: ${entity.sentiment.score}`);
+    console.log(`  Magnitude: ${entity.sentiment.magnitude}`);
+  });
+}
 
 client.login(config.token);
 
-
-client.on('message', (message) => {
+client.on('message', async function (message) {
+  await getSentiment(message.content)
 })
 
 client.on('ready', () => {
