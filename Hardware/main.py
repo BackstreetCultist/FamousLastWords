d960592e1e92ca4e297b25c2f7139842e0565ec3
BackstreetCultist <charlie@101a.co.uk>
2020-02-19 14:36:21 +0000

    Add updated hardware code from Sunshine

--- Hardware/main.py
+++ Hardware/main.py
@@ -2,7 +2,12 @@ from time import sleep
 import RPi.GPIO as GPIO
 import asyncio
 import websockets
-import constant
+
+#uri = "ws://lookalivesunshine.tech:80"
+
+uri ="ws://famouslastwords.dummybug.com:80"
+
+eyesopen = False
 
 #Don't touch these
 UPMOTOR = 2
@@ -12,45 +17,46 @@ DOWNMOTOR = 5
 
 def setGPIOs():
     GPIO.setmode(GPIO.BCM)
-    GPIO.setup(UPMOTOR, GPIO.OUT)
+    GPIO.setup(UPMOTOR, GPIO.IN)
     GPIO.setup(LEFTEYE, GPIO.OUT)
     GPIO.setup(RIGHTEYE, GPIO.OUT)
     GPIO.setup(DOWNMOTOR, GPIO.OUT)
 
 def act():
-        GPIO.output(UPMOTOR, False)
-        sleep(0.50)
+    global eyesopen
+    if not eyesopen:
+        GPIO.setup(UPMOTOR, GPIO.OUT)
         GPIO.output(UPMOTOR, True)
-
-        GPIO.output(LEFTEYE, True)
-        GPIO.output(RIGHTEYE, True)
-        sleep(5.00)
-        GPIO.output(LEFTEYE, False)
-        GPIO.output(RIGHTEYE, False)
-
-        GPIO.output(DOWNMOTOR, False)
         sleep(0.50)
-        GPIO.output(DOWNMOTOR, True)
+        GPIO.setup(UPMOTOR, GPIO.IN)
+        eyesopen = True
 
+    GPIO.output(LEFTEYE, True)
+    GPIO.output(RIGHTEYE, True)
+    sleep(5.00)
+    GPIO.output(LEFTEYE, False)
+    GPIO.output(RIGHTEYE, False)
 
-
+    GPIO.output(DOWNMOTOR, False)
+    sleep(0.50)
+    GPIO.output(DOWNMOTOR, True)
 
 async def start():
-    uri = "ws://lookalivesunshine.tech:81"
-    async with websockets.connect(uri) as websocket:
-        await websocket.send("pi")
-        await websocket.recv()
+    while True:
+        async with websockets.connect(uri) as websocket:
+            await websocket.recv()
+            print("Pi working")
+            act()
+        #GPIO.cleanup()
 
-        print("Pi working")
-        act()
 
-
-print("Setting GPIOs")
+#setGPIOs()
+#act();
+#GPIO.cleanup()
+    print("Setting GPIOs")
 setGPIOs()
-print("Sleeping")
-sleep(5)
-#asyncio.get_event_loop().run_until_complete(start())
-print("Acting")
-act()
-print("Done acting")
-GPIO.cleanup()
+try:
+    asyncio.get_event_loop().run_until_complete(start())
+except KeyboardInterrupt:
+    GPIO.cleanup()
+
